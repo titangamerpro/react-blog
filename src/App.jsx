ffba305d12514211ed03876b089dd4ff9bdb83e4
titangamerpro <titangamerpro10@gmail.com>
2025-04-10 15:20:36 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Home from "./components/Home"
+import Layout from "./components/Layout"
 
 import { posts } from "./utils/blog"
 
@@ -9,9 +10,7 @@ function App() {
 
   return (
     <>
-      <Header/>
-      <Home posts={posts} />
-      <Footer/>
+     <Layout posts={posts} />
     </>
   )
 }
