aa1ca0f791929bb838118f0a7abd1db5c4a8ef39
titangamerpro <titangamerpro10@gmail.com>
2025-06-14 10:27:30 +0200

    commit
    add page edit post

--- src/components/EditPost.jsx
+++ src/components/EditPost.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
+import notFound from '../assets/eror-404.svg'
 
 
 const EditPost = ({allPosts, setAllPosts}) => {
@@ -7,8 +8,8 @@ const EditPost = ({allPosts, setAllPosts}) => {
   const postToEdit = allPosts.find(post => post.id.toString() === id)
   const navigate = useNavigate()
 
-  const [editTitle, useEditTitle] = useState(postToEdit.title || "")
-  const [editBody, useEditBody] = useState(postToEdit.body || "")
+  const [editTitle, setEditTitle] = useState(postToEdit?.title || "")
+  const [editBody, setEditBody] = useState(postToEdit?.body || "")
   
   const handleSave = e => {
     e.preventDefault()
@@ -24,14 +25,37 @@ const EditPost = ({allPosts, setAllPosts}) => {
   }
 
     if (!postToEdit) {
-      return <p>not post</p>
+      return <main>
+        <img src={notFound} alt="eror" />
+        <p className='not-found'>not post</p>
+        </main>
     }
 
 
   // console.log(postToEdit);
   return (
     <main>
-      <h2>Edit Post</h2>
+      <h1>NewPost</h1> 
+        <form className="newFormPost" onSubmit={handleSave}>
+          <label htmlFor="postTitle">title:</label>
+          <input type="text" 
+              id='postTitle' 
+              required
+              value={editTitle}
+              placeholder='add the title'
+              onChange={(e) => setEditTitle(e.target.value) }
+          />
+          <label htmlFor="postBody">body:</label>
+          <textarea id='postBody' 
+                    value={editBody}
+                    placeholder='add the text'
+                    maxLength="500"
+                    onChange={(e) => setEditBody(e.target.value)}
+          />
+          <button type='submit'  >
+            submit
+          </button>
+        </form>
     </main>
   )
 }
